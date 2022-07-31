8f787c745c788384b4231a6e3e59a20f65c7da88
gou-zai <3201557995@qq.com>
2022-07-31 19:27:45 +0800

    新增 菜单 包括项 关于 退出 关于项存在闪退问题

--- app/src/main/java/com/gzy/mymicroide/MainActivity.kt
+++ app/src/main/java/com/gzy/mymicroide/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.gzy.mymicroide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.about_item -> startActivity(Intent(this,AboutActivity::class.java))
+            R.id.quit -> finish()
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
